b4e59ce8df695a52362aa6e71225809cca4c581e
AbsolutN <89788128+AbsolutN@users.noreply.github.com>
2021-09-16 11:42:10 +0200

    Update bintreeFile.py

--- bintreeFile.py
+++ bintreeFile.py
@@ -14,9 +14,9 @@ class Bintree:
         # Sorterar in newvalue i trädet
         self.root = putta(self.root,newvalue)
 
-    def __contains__(self,value):
+    def __contains__(self,searchedvalue):
         # True om value finns i trädet, False annars
-        return finns(self.root,value)
+        return finns(self.root,searchedvalue)
 
     def write(self):
         # Skriver ut trädet i inorder
@@ -26,21 +26,33 @@ class Bintree:
         
         
         
-def putta(root,newvalue):
-    p = Bintree.put()
+def putta(p,newvalue):
+    if p == None: 
+       return Node(newvalue)
+
+    elif newvalue < p.value:
+         if p.left == None:
+            p.left = Node(newvalue)
+         else:
+            putta(p.left, newvalue)
+              
+    elif newvalue > p.value:
+       if p.right == None:
+          p.right = Node(newvalue)
+       else:
+            putta(p.right,newvalue)
+    return p
     
     
 def finns(p,value):
-        letar = True
-        while letar:
-            if p == None: 
-                return False
-            if value == p.value: 
-                return True
-            if value < p.value: 
-                p = p.left
-            if value > p.value: 
-                p = p.right
+        if p == None: 
+            return False
+        if value == p.value: 
+            return True
+        if value < p.value: 
+            return finns(p.left,value)
+        if value > p.value: 
+            return finns(p.right,value)
                 
                 
 def skriv():
